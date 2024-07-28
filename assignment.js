2a3ad171bcbc64b915743187af1a223880577c66
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-07-28 17:25:22 +0200

    Add a default argument Assignment 3

--- assignment.js
+++ assignment.js
@@ -8,12 +8,26 @@
 const sayHello=(name)=>{
   console.log('Hi ' + name);
 }
+/**
+ * This function will print a message to the console no arguments are passed
+ */
 const sayHello1=()=>{
     console.log("Hi Simon");
 }
+/**
+ * This function will print a message to the console two arguments are passed
+ * @param sentence
+ * @param name
+ */
 const sayHello2=(sentence,name)=>{
   console.log(sentence + name);
 }
+
+const sayHello3=(sentence = "Hi",name)=>{
+    console.log(sentence + name);
+}
+
 sayHello("Simon");
 sayHello1();
 sayHello2("Hi ","Simon");
+sayHello("Elie");
