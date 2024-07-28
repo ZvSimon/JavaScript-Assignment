345ee79377032109e9efda64643c4cdb34660e66
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-07-28 17:17:32 +0200

    Assignment 1 and 2 down

--- assignment.js
+++ assignment.js
@@ -1,5 +1,19 @@
-function sayHello(name) {
+// function sayHello(name) {
+//   console.log('Hi ' + name);
+// }
+/**
+ * This function will print a message to the console one argument is passed
+ * @param name
+ */
+const sayHello=(name)=>{
   console.log('Hi ' + name);
 }
-
-sayHello();
+const sayHello1=()=>{
+    console.log("Hi Simon");
+}
+const sayHello2=(sentence,name)=>{
+  console.log(sentence + name);
+}
+sayHello("Simon");
+sayHello1();
+sayHello2("Hi ","Simon");
